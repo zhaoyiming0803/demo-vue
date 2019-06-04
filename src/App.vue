00d233133474d29c29970f07ca75635f60aed07b
zymseo <1047832475@qq.com>
2019-06-04 23:28:19 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,68 @@
 <template>
   <div class="app-container">
-    <hello></hello>
+    <div v-show="currentRuleTagConfig.config && currentRuleTagConfig.config.type === 2">
+      <div v-for="(v, k) in currentRuleTagConfig.config && currentRuleTagConfig.config.options" :key="k">
+        <input type="checkbox" :id="k" :value="v" v-model="currentRuleTagConfig.value">
+        <label :for="k">{{v}}</label>
+      </div>
+    </div>
+    <button @click="check">显示或隐藏</button>
+    <button @click="change">修改</button>
+    <button @click="log">打印</button>
   </div>
 </template>
 
 <script>
-  import hello from './components/hello';
-  import world from './components/world';
   export default {
     data () {
       return {
-        name: 'APP'
+        currentRuleTagConfig: {},
+        ruleTagList: [
+          {
+            index: 0,
+            config: {
+              type: 2,
+              options: ['男', '女', '未知']
+            }
+          },
+          {
+            index: 1,
+            config: {
+              type: 2,
+              options: ['北京', '杭州', '太原']
+            }
+          }
+        ],
+        isShowModal: false
       }
     },
 
-    components: {
-      hello,
-      world
-    },
+    methods: {
+      check () {
+        const index = this.currentRuleTagConfig.index || 0;
+        if (this.ruleTagList[index].value === undefined) {
+          this.$set(this.ruleTagList[index], 'value', []);
+        }
+        this.currentRuleTagConfig = this.ruleTagList[index];
+        this.isShowModal = !this.isShowModal;
+      },
 
-    created () {
-      
-    },
+      log () {
+        console.log(this.currentRuleTagConfig);
+      },
 
-    mounted () {
-      
+      change () {
+        const currentRuleTagConfig = this.currentRuleTagConfig.index === 1
+          ? this.ruleTagList[0]
+          : this.ruleTagList[1];
+        if (currentRuleTagConfig.value === undefined) {
+          this.$set(currentRuleTagConfig, 'value', []);
+        }
+        this.currentRuleTagConfig = currentRuleTagConfig;
+        console.log(this.currentRuleTagConfig);
+      }
     }
+    
   }
 </script>
 
