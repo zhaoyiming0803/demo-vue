cd2ca8331a7fef7a8fbbdea015e87085d064aecf
zhaoyiming <yiming_zhao@qingsongchou.com>
2019-06-10 00:15:33 +0800

    test: initWatch

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,72 @@
 <template>
   <div class="app-container">
-    <hello></hello>
+    <div>num1:{{num1}}</div>
+    <div>num2:{{num2}}</div>
+    <div>num3:{{num3}}</div>
+    <div>年龄：{{person.age}}</div>
+    <button @click="changeNum">生成随机数</button>
+    <button @click="changeAge">长大啦</button>
   </div>
 </template>
 
 <script>
-  import hello from './components/hello';
-  import world from './components/world';
   export default {
-    data () {
-      return {
-        name: 'APP'
+  data () {
+    return {
+      num1: 1,
+      num2: 2,
+      num3: 3,
+      person: {
+        name: 'zhaoyiming',
+        age: 18
       }
+    }
+  },
+
+  watch: {
+    num1 (n, o) {
+      console.log(n);
     },
 
-    components: {
-      hello,
-      world
+    num2: [
+      function handler (n, o) {
+        console.log('num2-1：', n);
+      },
+      {
+        handler (n, o) {
+          console.log('num2-2：', n);
+        }
+      },
+      'modifiedNum2'
+    ],
+
+    person: {
+      handler (n, o) {
+        console.log('长大啦：', n.age);
+      },
+      deep: true,
+      immediate: true
     },
 
-    created () {
-      
+    'person.age' (n, o) {
+      console.log('person.age:', n);
+    }
+  },
+
+  methods: {
+    changeNum () {
+      this.num1 = this.num2 = this.num3 = Math.random();
     },
 
-    mounted () {
-      
+    changeAge () {
+      this.person.age += 1;
+    },
+
+    modifiedNum2 (n, o) {
+      console.log('modifiedNum2:', n);
     }
   }
+}
 </script>
 
 <style>
